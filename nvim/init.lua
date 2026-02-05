eb51f306d19189d37b968aa0e25cfaacfa5b689b
xjoda256 <jdwhite9687@gmail.com>
2026-02-05 17:42:27 -0600

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,53 +1,109 @@
-local function bootstrap_pckr()
-  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+-----------------------------
+-- ~/.config/nvim/init.lua --
+-- ×Joda™ -------------------
+-----------------------------
 
-  if not (vim.uv or vim.loop).fs_stat(pckr_path) then
-    vim.fn.system({
-      'git',
-      'clone',
-      "--filter=blob:none",
-      'https://github.com/lewis6991/pckr.nvim',
-      pckr_path
-    })
-  end
+-- vim options --
+vim.o.number = true
+vim.o.showmatch = true
+vim.o.cursorline = true
+vim.o.swapfile = false
+vim.o.termguicolors = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = false
+vim.o.softtabstop = 2
+vim.o.signcolumn = "yes"
+vim.o.smartindent = true
+vim.o.winborder = "rounded"
 
-  vim.opt.rtp:prepend(pckr_path)
-end
+-- keybinds --
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>w", ":write<CR>")
+vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
+vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
+vim.keymap.set("n", "<leader>c", ":ColorizerToggle<CR>")
 
-bootstrap_pckr()
+-- plugins --
+vim.pack.add({
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.pick" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = "https://github.com/nvimdev/dashboard-nvim" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
+	{ src = "https://github.com/folke/snacks.nvim" },
+	-- colorschemes --
+	{ src = "https://github.com/neanias/everforest-nvim" },
+	{ src = "https://github.com/vague-theme/vague.nvim" },
+	{ src = "https://github.com/catppuccin/nvim" },
+	{ src = "https://github.com/folke/tokyonight.nvim" },
+	{ src = "https://github.com/Mofiqul/dracula.nvim" },
+	{ src = "https://github.com/rebelot/kanagawa.nvim" },
+	{ src = "https://github.com/sainnhe/sonokai" },
+	{ src = "https://github.com/norcalli/nvim-colorizer.lua" },
+	{ src = "https://github.com/uZer/pywal16.nvim" },
+})
 
--- Load plugins
-require('pckr').add{
-    'catppuccin/nvim';
-    'folke/snacks.nvim';
-    'folke/tokyonight.nvim';
-    'hrsh7th/nvim-cmp';
-    'steelsojka/pears.nvim';
-    'deviantfero/wpgtk.vim';
-    -- 'foo1/bar1.nvim';
-  -- 'foo2/bar2.nvim';
-}
+-- plugin setup --
+require("mini.pick").setup()
+require("oil").setup()
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		"lua_ls",
+		"stylua",
+		"pyright",
+		"texlab",
+		"bashls",
+		"marksman",
+		"rust-analyzer",
+		"phpactor",
+		"vim-language-server",
+		"tinymist",
+	},
+})
+require("dashboard").setup()
 
--- VIm commands
-vim.cmd [[set number]]
-vim.cmd [[set showmatch]]
-vim.cmd [[set cursorline]]
+-- enable language servers --
+vim.lsp.enable({
+	"lua_ls",
+	"bashls",
+	"marksman",
+	"rust-analyzer",
+	"gopls",
+	"phpactor",
+	"sql-language-server",
+	"vim-language-server",
+	"tinymist",
+})
 
--- Set the number of spaces a tab represents
-vim.opt.tabstop = 4
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+	},
+})
 
--- Set the number of spaces for each indentation (used by >>, <<, and auto-indent)
-vim.opt.shiftwidth = 4
+-- folding options --
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
 
--- When a tab is inserted, expand it to spaces
-vim.opt.expandtab = true
-
--- Set the number of spaces for a soft tab (used for editing; often same as shiftwidth)
-vim.opt.softtabstop = 4
-
--- Enable smart indentation (automatic indentation of new lines)
-vim.opt.smartindent = true
-
--- Colorscheme last to make sure all plugins are loaded
-vim.cmd [[colorscheme catppuccin-macchiato]]
+-- set colorscheme --
+vim.cmd([[colorscheme catppuccin-frappe]])
+vim.cmd([[highlight StatusLine none]])
 
